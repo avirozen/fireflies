ac53f5fa0b28be7155d75043e52e9770bdc5b952
avirozen <117524971+avirozen@users.noreply.github.com>
2022-12-24 11:08:46 +0200

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -5,18 +5,12 @@ let clock = 0
 radio.setTransmitPower(1)
 radio.setGroup(12)
 basic.forever(function () {
-    // if clock hits noon, flash the screen
     if (clock >= 8) {
-        // notify neighbors
         radio.sendNumber(0)
-        // flash
         game.addScore(1)
-        // wait for 2 ticks
         basic.pause(200)
-        // reset the clock
         clock = 0
     } else {
-        // just wait a bit
         basic.pause(100)
         clock += 1
     }
